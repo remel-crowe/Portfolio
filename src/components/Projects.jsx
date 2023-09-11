9955d136a3baba21ea1d9c3d9a5d363142d5415a
remcrw <84338244+remel-crowe@users.noreply.github.com>
2023-09-11 21:31:11 +0100

    circle component added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,62 +3,38 @@ import { AiFillGithub } from "react-icons/ai";
 import { VscLinkExternal } from "react-icons/Vsc";
 import { useState, useEffect } from "react";
 import logo from "../assets/mockup.jpg";
+import Circles from "./circles";
 
 function Projects() {
-  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
-
-  // Function to handle scroll and update the current project index
-  const handleScroll = () => {
-    const container = document.querySelector(".scrolling-container");
-    if (container) {
-      const scrollPosition = container.scrollLeft;
-      const projectWidth = container.clientWidth; // Width of each project card
-      const newIndex = Math.round(scrollPosition / projectWidth);
-      setCurrentProjectIndex(newIndex);
-    }
-  };
-
-  // Add a scroll event listener to track scroll position
-  useEffect(() => {
-    const container = document.querySelector(".scrolling-container");
-    if (container) {
-      container.addEventListener("scroll", handleScroll);
-    }
-    return () => {
-      if (container) {
-        container.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
   return (
     <div className="flex flex-col items-center relative md:mb-10">
       <h1 className="absolute top-24 mt-12 md:top-24 uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-lg md:text-2xl">
         Projects
       </h1>
-      <ul className="scrolling-container relative top-[300px] flex items-center overflow-x-scroll gap-x-[1000px] w-full snap-x snap-mandatory ">
-        <li className="scrolling-item flex items-center snap-center">
-          <div className="project-content z-10 w-[550px] h-[350px] p-8 rounded-lg ">
+      <ul className="scrolling-container relative top-[300px] flex items-center overflow-x-scroll sm:gap-x-[1000px] w-full snap-x snap-mandatory ">
+        <li className="scrolling-item flex flex-col md:flex-row items-center snap-center">
+          <div className="project-content z-10 w-full md:w-[550px] p-4 md:p-8 rounded-lg text-center md:text-left">
             <p className="text-yellow-400">Featured Project</p>
-            <h3 className="text-2xl font-bold">
+            <h3 className="text-2xl font-bold mt-2 md:mt-4">
               <a href="#" className="text-white">
                 Telegram Crypto Bot
               </a>
             </h3>
-            <div className="project-description mt-4 bg-gray-600 text-gray-200 p-4 shadow-lg rounded-md">
-              <p>
+            <div className="project-description mt-4 bg-gray-600 text-gray-200 p-2 md:p-4 shadow-lg rounded-md">
+              <p className="text-xs md:text-sm">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Reiciendis omnis animi impedit eum. Quia itaque omnis, culpa
                 voluptates saepe nostrum consectetur, inventore aliquam eaque
                 fuga totam! In minus doloribus consectetur?
               </p>
             </div>
-            <ul className="project-tech-list flex mt-4 space-x-4 text-gray-200 font-poppins text-sm">
-              <li>VS Code</li>
-              <li>Telegram</li>
-              <li>Python</li>
-              <li>CoinGecko API</li>
+            <ul className="project-tech-list mt-4 flex flex-wrap text-gray-200 font-poppins text-xs md:text-sm">
+              <li className="mb-2 md:mb-0 md:mr-4">VS Code</li>
+              <li className="mb-2 md:mb-0 md:mr-4">Telegram</li>
+              <li className="mb-2 md:mb-0 md:mr-4">Python</li>
+              <li className="mb-2 md:mb-0">CoinGecko API</li>
             </ul>
-            <div className="project-links mt-4 inline-flex space-x-3">
+            <div className="project-links mt-4 flex space-x-3">
               <a href="#" className="text-gray-100">
                 <AiFillGithub className="w-6 h-6" />
               </a>
@@ -67,15 +43,16 @@ function Projects() {
               </a>
             </div>
           </div>
-          <div className="project-image z-0 w-[350px] h-[300px] -ml-[150px] relative">
+          <div className="project-image z-0 w-full md:w-[350px] h-[300px] relative mt-4 md:mt-0">
             <img
               src="https://i.ibb.co/wSv13y9/hero.png"
               alt="Crypto bot"
               className="object-cover w-full h-full object-fill rounded-sm"
             />
-            <div className=" cursor-pointer absolute w-[350px] h-[300px] inset-0 bg-gray-700 opacity-30 transition-opacity duration-300 hover:opacity-0"></div>
+            <div className="cursor-pointer absolute w-full h-full inset-0 bg-gray-700 opacity-30 transition-opacity duration-300 hover:opacity-0"></div>
           </div>
         </li>
+
         <li className="scrolling-item flex items-center relative snap-center mx-auto">
           <div className="project-image z-0 w-[350px] h-[300px] -mr-[150px] relative">
             <img
@@ -162,16 +139,7 @@ function Projects() {
           </div>
         </li>
       </ul>
-      <div className="flex relative top-[350px]">
-        {[0, 1, 2].map((index) => (
-          <div
-            key={index}
-            className={`w-3 h-3 rounded-full ${
-              index === currentProjectIndex ? "bg-yellow-400" : "bg-gray-400"
-            } mr-2`}
-          ></div>
-        ))}
-      </div>
+      <Circles />
     </div>
   );
 }
