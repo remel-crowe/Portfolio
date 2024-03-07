70c4c692c59601277c2c1ed415a33005db14be5e
remel-crowe <remelscrowe@gmail.com>
2024-03-07 10:46:25 +0000

    minor updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,7 @@ function About() {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: false }}
           src="https://i.ibb.co/vmHYkGt/IMG-3125.jpg"
-          className=" w-40 h-40 mt-[170px] md:mt-[120px] flex-shrink-0 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[600px]"
+          className=" w-40 h-40 mt-[180px] md:mt-[120px] flex-shrink-0 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[600px]"
         ></motion.img>
         <div className="space-y-5 md:space-y-10 px-0 md:px-10 mb-[90px] md:pt-20 lg:mt-20">
           <h4 className="text-4xl">
@@ -34,7 +34,7 @@ function About() {
             </span>{" "}
             Introduction
           </h4>
-          <p className="font-poppins text-md md:text-lg text-slate-200 ">
+          <p className=" text-xs md:text-lg text-slate-200 ">
             Hey there! 👋 I&apos;m Remel, a frontend engineer passionate about
             crafting captivating online experiences. After nearly 4 years in the
             IT industry, I decided it was time for a change and made the bold
@@ -42,14 +42,14 @@ function About() {
             fueled by passion, curiosity and a desire to challenge myself in new
             ways.
           </p>
-          <p className="font-poppins text-md md:text-lg text-slate-200 ">
+          <p className=" text-xs md:text-lg text-slate-200 ">
             Well, talk about a rollercoaster ride! One day, I&apos;m deep diving
             into project refactoring and the next i&apos;m scratching my head
             over how to center a div. But hey, I guess its all part of the
             process, right? I can&apos;t wait to see whats around the corner for
             me and im on the lookout for that junior position!
           </p>
-          <p className="font-poppins text-md md:text-lg text-slate-200 ">
+          <p className=" text-xs md:text-lg text-slate-200 ">
             When i&apos;m not at work, i&apos;m probably on the move, exploring
             different corners of the world! My Goal? To visit 100 countries! My
             latest adventure? A Three-month solo trip through Southeast Asia.
--- src/components/Project/ProjectSec.jsx
+++ src/components/Project/ProjectSec.jsx
@@ -7,9 +7,9 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "Electric Vehicle Data Visualisation (WIP)",
+    title: "Batterypacked - EV Data Visualisation & Comparison Tool (WIP)",
     description:
-      "Utilises the Electric Vehicle Data API to fetch information about electric vehicles and allows users to search for specific vehicles. Currently implementing comparison features",
+      "My second project involving EV's. Safe to say I like them! This application leverages my Electric Vehicle Data API to fetch information about electric vehicles and displays it to users in a user friendly format. Its built in React.js and uses Sass for styling. It's in its early stages but I'm currently adding a comparison tool and a journey planner (with charging stations).",
     image: "https://i.ibb.co/QNwW6y8/Screenshot-2024-03-06-at-19-15-11.png",
     tag: ["All", "Frontend"],
     gitUrl: "https://github.com/remel-crowe/ev-data-front",
@@ -17,9 +17,9 @@ const projectsData = [
   },
   {
     id: 2,
-    title: "Netflix Clone",
+    title: "Not Netflix!",
     description:
-      "Crafted using React, this Netflix Clone utilises The Movie Database API to dynamically fetch movies. Styled with Sass, it offers an authenication flow through Google's Firebase ",
+      "Crafted using React, this Netflix Clone utilises The Movie Database API to dynamically fetch movies. Styled with Sass, it offers an authenication flow through Google's Firebase. Please be sure to use a dummy email and password when visiting the site!",
     image: "https://i.ibb.co/K50KqjW/Screenshot-2024-02-29-at-15-52-38.png",
     tag: ["All", "Frontend"],
     gitUrl: "https://github.com/remel-crowe/Netflix-Clone",
@@ -29,7 +29,7 @@ const projectsData = [
     id: 3,
     title: "Electric Vehicle Data API",
     description:
-      "Backend API for searching cars by model name, leveraging Node.js, Express.js, regular expressions, and MongoDB. This API provides a straightforward way for users to retrieve car data based on their specific search criteria.",
+      "Backend API for searching cars by model name, leveraging Node.js, Express.js, regular expressions, and MongoDB. This API provides a straightforward way for applications to retrieve car data based on a specific search criteria.",
     image: "https://i.ibb.co/zH8VbfV/Screenshot-2024-02-29-at-10-33-51.png",
     tag: ["All", "Backend"],
     gitUrl: "https://github.com/remel-crowe/ev-data-api",
@@ -37,8 +37,9 @@ const projectsData = [
   },
   {
     id: 4,
-    title: "Animated Image Carousel",
-    description: "Responsive image carousel built with HTML, JS and CSS",
+    title: "Electric Vehicle Image Carousel",
+    description:
+      "Built using HTML, CSS and Javascript, this responsive carousel provides seamless transitions between images and descriptive text. Fully customisable and available on my Github! ",
     image: "https://i.ibb.co/RH6f5dD/Screenshot-2024-02-15-at-22-07-48.png",
     tag: ["All", "Frontend"],
     gitUrl: "https://github.com/remel-crowe/Image-carousel",
@@ -46,9 +47,9 @@ const projectsData = [
   },
   {
     id: 5,
-    title: "This Website!",
+    title: "Remcrw.me",
     description:
-      "Responsive application built using Next.js to display personal projects",
+      "Pretty self explanatory but lets get into the technical details. Built using Next.js with styling powered by tailwind css. Its fully responsive and gave me a great intoduction to the framer-motion library. Constantly updating it, so be on the lookout for any updates. Feel free to use it as a base for your own Portfolio if you want!",
     image: "https://i.ibb.co/zFKBsXF/mockup.png",
     tag: ["All", "Frontend"],
     gitUrl: "https://github.com/remel-crowe/Portfolio",
@@ -58,7 +59,7 @@ const projectsData = [
     id: 6,
     title: "Battleships",
     description:
-      "Interactive Battleships game where you face off against the computer, built with HTML, CSS, and JavaScript.",
+      "Who doesn't love battleships? This implementation of battleships was a great way for me to work on my algorithmic skillset as part of 'The Odin Project' curriculum. Built using HTMl, CSS and Javascript it offers users a chance to face off against the computer in a game of battleships.",
     image: "https://i.ibb.co/8zbFMFb/Screenshot-2024-02-23-at-13-23-08.png",
     tag: ["All", "Frontend"],
     gitUrl: "https://github.com/remel-crowe/Battleships",
